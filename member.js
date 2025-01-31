f6a6cc8d187541e7281854126ff7ce752a68633e
Prags-Prak <pramadas@pekininsurance.com>
2025-01-31 20:22:16 +0000

    my latets commit

--- member.js
+++ member.js
@@ -1,8 +1,3 @@
 function skillsmember() {
-    var skill = document.getElementById("skills").value;
-    var skillList = document.getElementById("skillList").value;
-    var li = document.createElement("li");
-    var text = document.createTextNode(skill);
-    li.appendChild(text);
-    skillList.appendChild(li);
-}   
+    console.log('skillsmember');
+}
